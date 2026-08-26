08b9acbaf6100e65be5c24ec28dc57d5cdae8b5d
MarinaPimenova <mnfpma@gmail.com>
2026-08-26 17:17:44 +0300

    Init

---
+++ src/main/java/com/wk/ti/TiDocumentWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.wk.ti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiDocumentWorkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiDocumentWorkerApplication.class, args);
+    }
+
+}
